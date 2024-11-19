b18dc20ffcbd9c15abce141c9212357632195fd3
rohannstu <rohansinha141635@gmail.com>
2024-11-19 13:15:36 +0600

    done

--- src/main/java/com/java/springbatch/entity/Customer.java
+++ src/main/java/com/java/springbatch/entity/Customer.java
@@ -1,2 +1,42 @@
-package com.java.springbatch.entity;public class Customer {
+package com.java.springbatch.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "CUSTOMERS_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "GENDER")
+    private String gender;
+
+    @Column(name = "CONTACT")
+    private String contactNo;
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @Column(name = "DOB")
+    private String dob;
 }
